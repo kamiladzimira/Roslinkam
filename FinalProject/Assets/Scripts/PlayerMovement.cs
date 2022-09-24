5ed5deaae313f1a9e7d12418bba1c88efa702e2a
kamdzi <dzimiraka@gmail.com>
2022-09-24 20:26:54 +0200

    Player animation changes handled

--- FinalProject/Assets/Scripts/PlayerMovement.cs
+++ FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,36 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     Rigidbody2D rb2D;
     Vector2 movementVector;
+    Animator animator;
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     public void OnWalk(InputAction.CallbackContext context)
     {
        movementVector = context.ReadValue<Vector2>();
-       
     }
 
     private void OnMove()
     {
-        rb2D.MovePosition(rb2D.position + movementVector * moveSpeed * Time.fixedDeltaTime);
+        rb2D.MovePosition(rb2D.position + (movementVector * moveSpeed * Time.fixedDeltaTime));
+
+        if (movementVector.magnitude > Mathf.Epsilon)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("XSpeed", movementVector.x);
+            animator.SetFloat("YSpeed", movementVector.y);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        //if (movementVector.magnitude < 8 + Mathf.Epsilon && movementVector.magnitude > -8 - Mathf.Epsilon)
+
     }
 
     private void FixedUpdate()
