55b3971c45a734b07fea2a486d6d2e57ced098dd
kamdzi <dzimiraka@gmail.com>
2022-09-27 16:19:38 +0200

    Pickup items by the player handled

--- FinalProject/Assets/Scripts/Inventory.cs
+++ FinalProject/Assets/Scripts/Inventory.cs
@@ -6,4 +6,16 @@ using UnityEngine.InputSystem;
 public class Inventory : MonoBehaviour
 {
     
+    [SerializeField] GameObject inventory;
+
+    List<GameObject> pickups = new List<GameObject>();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pickup")
+        {
+            pickups.Add(collision.gameObject);
+            collision.gameObject.SetActive(false);
+            collision.transform.parent = inventory.transform;
+        }
+    }
 }
