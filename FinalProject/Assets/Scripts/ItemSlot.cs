94dd947fd572440676840841ec0a0af2755d53e3
kamdzi <dzimiraka@gmail.com>
2022-09-28 18:07:48 +0200

    Item slot default setup

--- FinalProject/Assets/Scripts/ItemSlot.cs
+++ FinalProject/Assets/Scripts/ItemSlot.cs
@@ -6,9 +6,25 @@ using UnityEngine.UI;
 public class ItemSlot : MonoBehaviour
 {
     [SerializeField] Image image;
+    private Item item;
+    public Item Item => item;
 
-    public void Setup(Sprite sprite)
+    public void Setup(Item item)
     {
-        image.sprite = sprite;
+        if (item == null)
+        {
+            Setup();
+            return;
+        }
+        image.sprite = item.Sprite;
+        image.gameObject.SetActive(true);
+        this.item = item;
+    }
+
+    public void Setup()
+    {
+        image.sprite = null;
+        image.gameObject.SetActive(false);
+        item = null;
     }
 }
