7124f3c6f22f91dfc69b6a61b70d0987c7b7a831
kamdzi <dzimiraka@gmail.com>
2022-09-28 18:23:22 +0200

    Seed type of item added

---
+++ FinalProject/Assets/Scripts/Seed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : Item
+{
+    public override void Use()
+    {
+        Debug.Log("Seed was used!");
+    }
+}
