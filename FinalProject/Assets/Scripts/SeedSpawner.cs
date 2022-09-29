abd4136da2313b57326fba8a7d27c331b98a7b19
kamdzi <dzimiraka@gmail.com>
2022-09-29 21:37:40 +0200

    Component responsible for spawner view added

--- FinalProject/Assets/Scripts/SeedSpawner.cs
+++ FinalProject/Assets/Scripts/SeedSpawner.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SeedSpawner : MonoBehaviour
 {
-    [SerializeField] private Seed seed;
+    [SerializeField] private List <Seed> seeds;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] public Action<float> onTimerChangedAction;
+
+    [SerializeField] float defaultTimer = 5f;
+    private float timer;
 
     private Seed currentSeed;
 
-    private void Awake()
+    private void Start()
     {
+        onTimerChangedAction?.Invoke(timer);
+    }
 
-        
+    private void Update()
+    {
+        ProcessTimer();
+        Debug.Log(timer);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,10 +44,22 @@ public class SeedSpawner : MonoBehaviour
     {
         PlayerMovement playerMovement = collision.GetComponent<PlayerMovement>();
 
-        if (playerMovement == null || currentSeed != null)
+        if (playerMovement == null || currentSeed != null || timer > 0)
         {
             return;
         }
-        currentSeed = Instantiate(seed, spawnPoint.position, Quaternion.identity, null);
+        
+        currentSeed = Instantiate(seeds[UnityEngine.Random.Range(0, seeds.Count)], spawnPoint.position, Quaternion.identity, null);
+        timer = defaultTimer;
+    }
+
+    private void ProcessTimer()
+    {
+        if (timer <= 0)
+        {
+            return;
+        }
+        timer -= Time.deltaTime;
+        onTimerChangedAction?.Invoke(timer);
     }
 }
---
+++ FinalProject/Assets/Scripts/SpawnerView.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpawnerView : MonoBehaviour
+{
+    [SerializeField] private SeedSpawner seedSpawner;
+    [SerializeField] TextMeshProUGUI timer;
+
+
+    private void OnEnable()
+    {
+        seedSpawner.onTimerChangedAction += OnTimerChanged;
+    }
+
+    private void OnDisable()
+    {
+        seedSpawner.onTimerChangedAction -= OnTimerChanged;
+    }
+
+    private void OnTimerChanged(float timerValue)
+    {
+        if (timerValue <= 0)
+        {
+            timerValue = 0;
+            timer.text = "Ready!";
+            return;
+        }
+        timer.text = timerValue.ToString("0.00");
+    }
+}
