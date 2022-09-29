9d29fbd6a657d079ece6dce5e3c32590b25c30f8
kamdzi <dzimiraka@gmail.com>
2022-09-29 12:04:06 +0200

    Logic for planting created

--- FinalProject/Assets/Scripts/Seed.cs
+++ FinalProject/Assets/Scripts/Seed.cs
@@ -4,8 +4,66 @@ using UnityEngine;
 
 public class Seed : Item
 {
+    private List <Farmland> farmlands = new List<Farmland>();
+
+    [SerializeField]private float growingTime;
+    [SerializeField]private Item crop;
+
+    public float GrowingTime => growingTime;
+    public Item Crop => crop;
     public override void Use()
     {
-        Debug.Log("Seed was used!");
+        if (farmlands.Count <= 0)
+        {
+            return;
+        }
+
+        Farmland farmland = GetClosestEmptyFarmland();
+
+        if (farmland == null)
+        {
+            return;
+        }
+
+        farmland.SeedPlant(this);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Farmland collisionFarmland = collision.GetComponent<Farmland>();
+        if (collisionFarmland == null)
+        {
+            return;
+        }
+
+        farmlands.Add(collisionFarmland);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Farmland collisionFarmland = collision.GetComponent<Farmland>();
+        if (collisionFarmland == null || !farmlands.Contains(collisionFarmland))
+        {
+            return;
+        }
+        farmlands.Remove(collisionFarmland);
+    }
+
+    private Farmland GetClosestEmptyFarmland()
+    {
+        Farmland result = null;
+        float distance = float.MaxValue;
+
+        for (int i = 0; i < farmlands.Count; i++)
+        {
+            float currentDistance = Vector3.Distance(transform.position, farmlands[i].transform.position);
+
+            if (currentDistance < distance && farmlands[i].IsEmpty)
+            {
+                distance = currentDistance;
+                result = farmlands[i];
+            }
+        }
+        return result;
     }
 }
