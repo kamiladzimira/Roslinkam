b34672756c6cad7af002b0c6660f282920669595
kamdzi <dzimiraka@gmail.com>
2022-10-23 14:53:43 +0200

    Logic for shop inventory created

---
+++ Roslinkam/Assets/Scripts/Shop.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private GameObject shopInventoryPanel;
+    [SerializeField] private GameObject playerInventoryPanel;
+    [SerializeField] private List<ItemSlot> itemSlots;
+    [SerializeField] List<Item> buyableItems = new List<Item>();
+    private Inventory inventory;
+
+    private bool isEmpty = true;
+    public bool IsEmpty => isEmpty;
+    
+
+    private ItemSlot selectedItemSlot;
+
+    void Start()
+    {
+        shopInventoryPanel.SetActive(false);
+        playerInventoryPanel.SetActive(false);
+    }
+
+    public void TriggerShop()
+    {
+        shopInventoryPanel.SetActive(!shopInventoryPanel.activeSelf);
+        playerInventoryPanel.SetActive(!playerInventoryPanel.activeSelf);
+        SetupShopSlots();
+    }
+
+    public void SetupShopSlots()
+    {
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            ItemSlot itemSlot = itemSlots[i];
+
+            if (i < buyableItems.Count)
+            {
+                Item item = buyableItems[i];
+                itemSlot.Setup(item);
+            }
+            else
+            {
+                itemSlot.Setup();
+            }
+        }
+
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            itemSlots[i].Select(false);
+        }
+    }
+
+    public void SetupPlayerInventorySlots()
+    {
+        inventory.SetupSlots();
+    }
+
+    public void Select(ItemSlot itemSlot)
+    {
+        if (itemSlot.Item == null)
+        {
+            return;
+        }
+        selectedItemSlot = itemSlot;
+        foreach (ItemSlot slot in itemSlots)
+        {
+            if (slot == selectedItemSlot)
+            {
+                slot.Select(true);
+            }
+            else
+            {
+                slot.Select(false);
+            }
+        }
+    }
+
+    public void BuySelectedItem()
+    {
+        if (selectedItemSlot == null)
+        {
+            return;
+        }
+        Drop(selectedItemSlot.Item);
+    }
+
+    public void Drop(Item item)
+    {
+        if (item == null)
+        {
+            return;
+        }
+
+        buyableItems.Remove(item);
+        Destroy(item.gameObject);
+        SetupShopSlots();
+    }
+}
---
+++ Roslinkam/Assets/Scripts/ShopActivator.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ShopActivator : MonoBehaviour
+{
+    private List<Shop> shopItems = new List<Shop>();
+    private Shop openShop;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Shop collisionShop = collision.GetComponent<Shop>();
+        if (collisionShop == null)
+        {
+            return;
+        }
+
+        shopItems.Add(collisionShop);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Shop collisionShop = collision.GetComponent<Shop>();
+        if (collisionShop == null || !shopItems.Contains(collisionShop))
+        {
+            return;
+        }
+        shopItems.Remove(collisionShop);
+    }
+
+    public void OnShopOpen(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+        {
+            return;
+        }
+
+        if (openShop != null)
+        {
+            openShop.TriggerShop();
+            openShop = null;
+            return;
+        }
+
+        if (shopItems.Count <= 0)
+        {
+            return;
+        }
+
+        openShop = GetClosestShop();
+        openShop.TriggerShop();
+    }
+
+    private Shop GetClosestShop()
+    {
+        Shop result = null;
+        float minDistance = float.MaxValue;
+
+        for (int i = 0; i < shopItems.Count; i++)
+        {
+            float currentDistance = Vector3.Distance(transform.position, shopItems[i].transform.position);
+
+            if (currentDistance < minDistance && shopItems[i].IsEmpty)
+            {
+                minDistance = currentDistance;
+                result = shopItems[i];
+            }
+        }
+        return result;
+    }
+}
