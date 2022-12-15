51f12fcb4e25f7808abad78789251cd7f7590442
kamdzi <dzimiraka@gmail.com>
2022-12-15 11:08:48 +0100

    Script for inventory at the bottom of the scene

---
+++ Roslinkam/Assets/Scripts/InventoryView.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryView : MonoBehaviour
+{
+    [SerializeField] private Inventory inventory;
+    [SerializeField] private List<ItemSlot> itemSlots;
+    private ItemSlot selectedItemSlot;
+
+    public void Select(ItemSlot itemSlot)
+    {
+        if (itemSlot.Item == null)
+        {
+            return;
+        }
+        selectedItemSlot = itemSlot;
+        foreach (ItemSlot slot in itemSlots)
+        {
+            if (slot == selectedItemSlot)
+            {
+                slot.Select(true);
+            }
+            else
+            {
+                slot.Select(false);
+            }
+        }
+    }
+
+    public void EquipSelectedItem()
+    {
+        if (selectedItemSlot == null)
+        {
+            return;
+        }
+
+        inventory.Equip(selectedItemSlot);
+    }
+
+    public void DropSelectedItem()
+    {
+        if (selectedItemSlot == null)
+        {
+            return;
+        }
+        inventory.Drop(selectedItemSlot.Item);
+    }
+
+    public void SetupSlots(List<Item> pickups)
+    {
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            ItemSlot itemSlot = itemSlots[i];
+
+            if (i < pickups.Count)
+            {
+                Item item = pickups[i];
+                itemSlot.Setup(item);
+            }
+            else
+            {
+                itemSlot.Setup();
+            }
+        }
+
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            itemSlots[i].Select(false);
+        }
+
+        selectedItemSlot = null;
+    }
+
+    /*public void SetupSlots(List<ItemContainer> itemContainers)
+    {
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            ItemSlot itemSlot = itemSlots[i];
+
+            if (i < itemContainers.Count)
+            {
+                ItemContainer itemContainer = itemContainers[i];
+                itemSlot.Setup(itemContainer);
+            }
+            else
+            {
+                itemSlot.Setup();
+            }
+        }
+
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            itemSlots[i].Select(false);
+        }
+
+        selectedItemSlot = null;
+    }*/
+}
+
