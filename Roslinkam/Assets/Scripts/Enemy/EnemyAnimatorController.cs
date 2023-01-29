9cdbc93b778ce5eedb04de7b60c6e0a4c6027ea6
kamdzi <dzimiraka@gmail.com>
2023-01-29 18:26:35 +0100

    Method for resetting all triggers

--- Roslinkam/Assets/Scripts/Enemy/EnemyAnimatorController.cs
+++ Roslinkam/Assets/Scripts/Enemy/EnemyAnimatorController.cs
@@ -6,11 +6,19 @@ using UnityEngine.Events;
 
 public class EnemyAnimatorController : MonoBehaviour
 {
+    [SerializeField] private Animator enemyAnimator;
     [SerializeField]
     private UnityEvent onAttack;
     public void OnAttack()
     {
         onAttack?.Invoke();
+    }
+
+    public void ResetAllTriggers()
+    {
+        enemyAnimator.ResetTrigger("Trigger");
+        enemyAnimator.ResetTrigger("Walk");
+        enemyAnimator.ResetTrigger("Attack");
     }
 }
 
