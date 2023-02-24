cd059c19b1d52944542e08131625dac365f023fb
kamdzi <dzimiraka@gmail.com>
2023-02-24 19:53:36 +0100

    Bullet rotation handled

--- Roslinkam/Assets/Scripts/Enemy/Bullet.cs
+++ Roslinkam/Assets/Scripts/Enemy/Bullet.cs
@@ -9,12 +9,13 @@ using UnityEngine.Subsystems;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private int _speed;
+    [SerializeField] private int _rotationSpeed;
     [SerializeField] private int _damage;
+    [SerializeField] private float _followTime = 2;
+    [SerializeField] private float _lifeTime = 6;
     private EnemyComponentsContainer _enemyComponentsContainer;
     private HealthController _target;
     private float _timer = 0;
-    private float _followTime = 2;
-    private float _lifeTime = 3;
     private Vector3 _direction;
 
     public int Damage => _damage;
@@ -22,6 +23,8 @@ public class Bullet : MonoBehaviour
     private void Update()
     {
         _timer += Time.deltaTime;
+        CalculateRotation();
+        ChangeRotation();
         MoveBullet(_speed);
         if (_timer > _lifeTime)
         {
@@ -38,16 +41,22 @@ public class Bullet : MonoBehaviour
     {
         _enemyComponentsContainer = container;
         _target = _enemyComponentsContainer.EnemyTargetFinder.Target;
+        CalculateRotation();
+        transform.rotation = Quaternion.LookRotation(Vector3.back, _direction);
     }
 
     private void MoveBullet(int speed)
+    {
+        Vector3 newPosition = transform.position + transform.up * speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+
+    private void CalculateRotation()
     {
         if (_timer <= _followTime)
         {
             _direction = (_target.transform.position - transform.position).normalized;
         }
-        Vector3 newPosition = transform.position + _direction * speed * Time.deltaTime;
-        transform.position = newPosition;
     }
 
     private void HandleDealDamage(Collider2D collision)
@@ -60,5 +69,12 @@ public class Bullet : MonoBehaviour
         }
         target.GetDamage(_damage);
         Destroy(gameObject);
+    }
+
+    private void ChangeRotation()
+    {
+        Quaternion rotationTarget = Quaternion.LookRotation(Vector3.back, _direction);
+        Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, rotationTarget, _rotationSpeed * Time.deltaTime);
+        transform.rotation = newRotation;
     }
 }
