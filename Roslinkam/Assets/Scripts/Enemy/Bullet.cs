a267a8945a9eace4e6b75bcb07ecddea1c0202a2
kamdzi <dzimiraka@gmail.com>
2023-02-24 18:45:01 +0100

    Move the bullet, get damage to the target, destroy the bullet

--- Roslinkam/Assets/Scripts/Enemy/Bullet.cs
+++ Roslinkam/Assets/Scripts/Enemy/Bullet.cs
@@ -1,37 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking.Types;
+using UnityEngine.Serialization;
+using UnityEngine.Subsystems;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] int speed;
-    [SerializeField] int damage;
-    private EnemyComponentsContainer enemyComponentsContainer;
-    private HealthController target;
+    [SerializeField] private int _speed;
+    [SerializeField] private int _damage;
+    private EnemyComponentsContainer _enemyComponentsContainer;
+    private HealthController _target;
+    private float _timer = 0;
+    private float _followTime = 2;
+    private float _lifeTime = 3;
+    private Vector3 _direction;
 
-    public int Damage => damage;
+    public int Damage => _damage;
 
     private void Update()
     {
-        MoveBullet(speed);
+        _timer += Time.deltaTime;
+        MoveBullet(_speed);
+        if (_timer > _lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         HandleDealDamage(collision);
-
     }
 
     public void Setup(EnemyComponentsContainer container)
     {
-        enemyComponentsContainer = container;
-        target = enemyComponentsContainer.EnemyTargetFinder.Target;
+        _enemyComponentsContainer = container;
+        _target = _enemyComponentsContainer.EnemyTargetFinder.Target;
     }
 
     private void MoveBullet(int speed)
     {
-        transform.position = Vector2.MoveTowards(transform.position,
-            target.transform.position, Time.deltaTime * speed);
+        if (_timer <= _followTime)
+        {
+            _direction = (_target.transform.position - transform.position).normalized;
+        }
+        Vector3 newPosition = transform.position + _direction * speed * Time.deltaTime;
+        transform.position = newPosition;
     }
 
     private void HandleDealDamage(Collider2D collision)
@@ -42,8 +58,7 @@ public class Bullet : MonoBehaviour
         {
             return;
         }
-        target.GetDamage(damage);
+        target.GetDamage(_damage);
         Destroy(gameObject);
     }
-
 }
